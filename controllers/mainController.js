8d5ded790d6288687ee899b03098259a168cd3e0
L00cky <kollins@windowslive.com>
2015-07-10 16:52:28 +0200

    all

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,7 +4,7 @@ app.controller('PhoneListCtrl', function ($scope, $http) {
       load_pictures();
 
     function load_pictures(){
-        $http.get('http://localhost:3000/load').success(function(data){
+        $http.get('https://enigmatic-harbor-2756.herokuapp.com/load').success(function(data){
             $scope.smartphones=data;
         });
     };
