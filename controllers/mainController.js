93774619181cf4ad27704eeae808de29074fdc2f
L00cky <kollins@windowslive.com>
2015-07-10 16:39:03 +0200

    test

---
+++ controllers/mainController.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("myApp", []);
+
+app.controller('PhoneListCtrl', function ($scope, $http) {
+      load_pictures();
+
+    function load_pictures(){
+        $http.get('http://localhost:3000/load').success(function(data){
+            $scope.smartphones=data;
+        });
+    };
+});
--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 var express = require('express');
+var mysql = require("mysql");
 var app = express();
+
+/*
+  * Configure MySQL parameters.
+*/
+var connection = mysql.createConnection({
+    host: "us-cdbr-iron-east-02.cleardb.net",
+    user: "b9a54a14d41bbe",
+    password: "91090f06",
+    database: "heroku_a3a793583554b25"
+});
+
+connection.connect(function (error) {
+    if (error) {
+        console.log("Problem with MySQL" + error);
+    }
+    else {
+        console.log("Connected with Database");
+    }
+});
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use("/js", express.static(__dirname + '/js'));
 app.use("/css", express.static(__dirname + '/css'));
+app.use("/controllers", express.static(__dirname + '/controllers'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -11,6 +33,17 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index')
+});
+
+app.get('/load', function (req, res) {
+    connection.query("SELECT * from smartphones", function (err, rows) {
+        if (err) {
+            console.log("Problem with MySQL" + err);
+        }
+        else {
+            res.end(JSON.stringify(rows));
+        }
+    });
 });
 
 app.listen(app.get('port'), function() {
