08a1e65e2729846763de1202adb392faa15f1727
L00cky <kollins@windowslive.com>
2015-07-11 12:26:03 +0200

    a

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/load', function (req, res) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port: ', app.get('port'));
 });
 
 
