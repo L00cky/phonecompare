edda8ad104a5e59da196fc4eeee7a69465e82cb6
L00cky <kollins@windowslive.com>
2015-07-11 15:31:11 +0200

    index.js error

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,7 +4,7 @@ app.controller('PhoneListCtrl', function ($scope, $http) {
       load_pictures();
 
     function load_pictures(){
-        $http.get('https://phone-compare.herokuapp.com//load').success(function (data) {
+        $http.get('https://phone-compare.herokuapp.com/load').success(function (data) {
             $scope.smartphones=data;
         });
     };
