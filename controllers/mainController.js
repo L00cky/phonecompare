d8637e59fb5f6a880e045ad8edc08e56638c6d18
L00cky <kollins@windowslive.com>
2015-07-11 21:03:40 +0200

    filters

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -8,4 +8,25 @@ app.controller('PhoneListCtrl', function ($scope, $http) {
             $scope.smartphones=data;
         });
     };
+
+    $scope.manufacturerIncludes = [];
+    
+    $scope.includeManufacturer = function(manufacturer) {
+        var i = $.inArray(manufacturer, $scope.manufacturerIncludes);
+        if (i > -1) {
+            $scope.manufacturerIncludes.splice(i, 1);
+        } else {
+            $scope.manufacturerIncludes.push(manufacturer);
+        }
+    }
+    
+    $scope.manufacturerFilter = function(manufacturer) {
+        if ($scope.manufacturerIncludes.length > 0) {
+            if ($.inArray(manufacturer.manufacturer, $scope.manufacturerIncludes) < 0)
+                return;
+        }
+        
+        return manufacturer;
+    }
+}
 });
