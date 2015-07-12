ae2f66a159bbafbb9780ac629fc54077e0695284
L00cky <kollins@windowslive.com>
2015-07-12 16:59:25 +0200

    c

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,38 +1,39 @@
 ﻿var app = angular.module("myApp", []);
 
 app.controller('PhoneListCtrl', function ($scope, $http) {
-    load_smartphones();
+    $scope.search = [];
+    $scope.smartphones = [
+                            {
+                                "id": 2,
+                                "manufacturer": "Umi",
+                                "name": "Iron",
+                                "snippet": "Best china ever",
+                                "price": 179.99,
+                                "img": "http://img.sunsky-online.com/upload/store/product_l/S-MPH-0971H.jpg"
+                            },
+                            {
+                                "id": 3,
+                                "manufacturer": "Samsung",
+                                "name": "Galaxy S6 Edge",
+                                "snippet": "Edged display",
+                                "price": 449.99,
+                                "img": "http://bestcena.cz/images/SAMSUNG%20galaxy%20S6%20edge%20black.jpg"
+                            },
+                            {
+                                "id": 12,
+                                "manufacturer": "Lenovo",
+                                "name": "K3 Note",
+                                "snippet": null,
+                                "price": 200,
+                                "img": "http://www.mixeshop.com/images/v/201502/14228507080.jpg"
+                            }
+                        ];
 
-    function load_smartphones() {
-        $http.get('https://phone-compare.herokuapp.com/load_smartphones').success(function (data) {
-            $scope.search = [];
-            //$scope.smartphones=data;
-            $scope.smartphones = [
-    {
-        "id": 2,
-        "manufacturer": "Umi",
-        "name": "Iron",
-        "snippet": "Best china ever",
-        "price": 179.99,
-        "img": "http://img.sunsky-online.com/upload/store/product_l/S-MPH-0971H.jpg"
-    },
-    {
-        "id": 3,
-        "manufacturer": "Samsung",
-        "name": "Galaxy S6 Edge",
-        "snippet": "Edged display",
-        "price": 449.99,
-        "img": "http://bestcena.cz/images/SAMSUNG%20galaxy%20S6%20edge%20black.jpg"
-    },
-    {
-        "id": 12,
-        "manufacturer": "Lenovo",
-        "name": "K3 Note",
-        "snippet": null,
-        "price": 200,
-        "img": "http://www.mixeshop.com/images/v/201502/14228507080.jpg"
-    }
-            ]
-        });
-    };
+    //load_smartphones();
+
+    //function load_smartphones() {
+    //    $http.get('https://phone-compare.herokuapp.com/load_smartphones').success(function (data) {
+    //        
+    //    })
+    //};
 });
