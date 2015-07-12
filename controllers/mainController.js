ddc599f1164f8cf42db1448eeb8e8c96a40a1c8d
L00cky <kollins@windowslive.com>
2015-07-12 11:12:11 +0200

    a

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,10 +3,36 @@
 app.controller('PhoneListCtrl', function ($scope, $http) {
     load_smartphones();
 
-    function load_smartphones(){
+    function load_smartphones() {
         $http.get('https://phone-compare.herokuapp.com/load_smartphones').success(function (data) {
             $scope.search = [];
-            $scope.smartphones=data;
+            //$scope.smartphones=data;
+            $scope.smartphones = [
+    {
+        "id": 2,
+        "manufacturer": "Umi",
+        "name": "Iron",
+        "snippet": "Best china ever",
+        "price": 179.99,
+        "img": "http://img.sunsky-online.com/upload/store/product_l/S-MPH-0971H.jpg"
+    },
+    {
+        "id": 3,
+        "manufacturer": "Samsung",
+        "name": "Galaxy S6 Edge",
+        "snippet": "Edged display",
+        "price": 449.99,
+        "img": "http://bestcena.cz/images/SAMSUNG%20galaxy%20S6%20edge%20black.jpg"
+    },
+    {
+        "id": 12,
+        "manufacturer": "Lenovo",
+        "name": "K3 Note",
+        "snippet": null,
+        "price": 200,
+        "img": "http://www.mixeshop.com/images/v/201502/14228507080.jpg"
+    }
+            ]
         });
     };
 });
